69485e23d2c9fae6aa5bb0e33c76417d9c670387
Sarit Singratanakul <you@example.com>
2023-03-22 14:21:16 +0700

    new update 4

--- src/components/blog1/Blog1.jsx
+++ src/components/blog1/Blog1.jsx
@@ -13,7 +13,7 @@ const Blog1 = () => {
       }, []);
   return (
     <>
-    <div className="blogMain">
+    <div className="blogMainNew">
     <div className='article'>
       <header>
         <div>March 16, 2023</div>
